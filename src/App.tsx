2f8e3f9d71dc60b6d715d97a1731d87251cdecf5
Sam Goldrick <sam@Sams-MacBook-Pro-2.local>
2024-07-09 19:18:41 +0100

    add expenselist and implement in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "test", amount: 7, category: "testCat" },
+    { id: 2, description: "test", amount: 7, category: "testCat" },
+    { id: 3, description: "test", amount: 7, category: "testCat" },
+    { id: 4, description: "test", amount: 7, category: "testCat" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
   return (
     <div className="w-screen h-screen flex flex-row">
-      <div className="h-max border border-red-100 m-4 p-4 rounded"></div>
+      <div className="h-max border border-red-100 m-4 p-4 rounded">
+        <ExpenseList expenses={expenses} onDelete={handleDelete}></ExpenseList>
+      </div>
     </div>
   );
 }
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,48 @@
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>description</th>
+            <th>amount</th>
+            <th>category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.id}</td>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button
+                  className="btn btn-error"
+                  onClick={() => onDelete(expense.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseList;
